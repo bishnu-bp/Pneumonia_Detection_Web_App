cf6da23150a0a89332e7244b621dcb20cf609fb7
Bishnu.np <bishnuupadhyaya590@gmail.com>
2025-12-09 20:33:20 +0545

    Updating requerment.txt

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import "../Styles/Navbar.css";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+
+function Navbar() {
+  const [nav, setNav] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const openNav = () => {
+    setNav(!nav);
+  };
+
+  const handleChatBtnClick = () => {
+    if (!isButtonDisabled) {
+      toast.info("Experiencing high traffic, Please wait a moment.", {
+        position: toast.POSITION.TOP_CENTER,
+        onOpen: () => setIsButtonDisabled(true),
+        onClose: () => setIsButtonDisabled(false),
+      });
+    }
+  };
+
+  return (
+    <div className="navbar-section">
+      <h1 className="navbar-title">
+        <Link to="/">
+          Radio <span className="navbar-sign">Scan</span>
+        </Link>
+      </h1>
+
+      {/* Desktop */}
+      <ul className="navbar-items">
+        <li>
+          <Link to="/" className="navbar-links">
+            Home
+          </Link>
+        </li>
+        <li>
+          <a href="#services" className="navbar-links">
+            Services
+          </a>
+        </li>
+      </ul>
+
+      <button
+        className="navbar-btn"
+        type="button"
+        disabled={isButtonDisabled}
+        onClick={handleChatBtnClick}
+      >
+         Login 
+      </button>
+
+      {/* Mobile */}
+      <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+        <div onClick={openNav} className="mobile-navbar-close">
+        </div>
+
+        <ul className="mobile-navbar-links">
+          <li>
+            <Link onClick={openNav} to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <a onClick={openNav} href="#services">
+              Services
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger Icon */}
+      
+    </div>
+  );
+}
+
+export default Navbar;
