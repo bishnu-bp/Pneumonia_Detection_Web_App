ea0bc46b98b6d5b6b9aa7ce999b6e13e37699a98
Bishnu.np <bishnuupadhyaya590@gmail.com>
2025-12-11 12:24:07 +0545

    Adding login and register page

--- backend/coursework/api/urls.py
+++ backend/coursework/api/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from .views import predict_image
+from django.urls import path
+from .views import RegisterView, UserView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenObtainPairView,
+)
 
 urlpatterns = [
     path("predict/", predict_image),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("user/", UserView.as_view(), name="user"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
 ]
+
--- backend/coursework/api/views.py
+++ backend/coursework/api/views.py
@@ -83,3 +83,24 @@ def predict_image(request):
         "temp1": counters["temp1"],
         "temp2": counters["temp2"]
     })
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            "username": user.username,
+            "email": user.email
+        })
